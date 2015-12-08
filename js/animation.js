2d1b42c603856abb5f11ed639431a4214ca8569c
danagilliann <danalee96@gmail.com>
2015-12-08 00:06:05 -0500

    animation working + started on links

--- js/animation.js
+++ js/animation.js
@@ -1,53 +1,68 @@
 $(document).ready(function() {
+  /* Hide all containers upon loading*/
+  // $('#about-container').hide();
 
-	$('body').on('click', '#about', function() {
+  /*
+   * Window changes 
+   * Have different height for different window.widths
+   */
+  $(window).scroll(function() { 
+    var height = $(window).scrollTop();
+    if (height > 200) {
+      $('.anim-description').addClass('animated fadeInUp');
+      $('.hello-world').addClass('animated fadeInUp');
+      $('p.about-description').addClass('animated fadeInUp');
+    }
 
-	$('html, body').animate({
-	      scrollTop: $(".hello-world").offset().top
-	  }, 500);
-	  return false;
-	});
-	
-	$('body').on('click', '#links', function() {
-	  $('html, body').animate({
-	      scrollTop: $("#links-header").offset().top
-	  }, 500);
-	  return false;
-	});	
+  });
 
-	$('body').on('click', '#projects', function() {
-	  $('html, body').animate({
-	      scrollTop: $("#projects-header").offset().top
-	  }, 500);
-	  return false;
-	});	
-
-	$('body').on('click', '#contact', function() {
-	  $('html, body').animate({
-	      scrollTop: $("#contact-header").offset().top
-	  }, 500);
-	  return false;
-	});	
-
-	$('body').on('click', '#portfolio', function() {
-	  $('html, body').animate({
-	      scrollTop: $("#portfolio-header").offset().top
-	  }, 500);
-	  return false;
-	});	
-
-	$('body').on('mouseenter mouseleave', 'img.main-photo-1', function() {
-	  $('img.main-photo-1').attr({
-	    src: $(this).attr('data-photoone-src')
-	    , 'data-photoone-src': $(this).attr('src'),
-	  });
-	});	
-
-	$('body').on('mouseenter mouseleave', 'img.main-photo-2', function() {
-	  $('img.main-photo-2').attr({
-	    src: $(this).attr('data-phototwo-src')
-	    , 'data-phototwo-src': $(this).attr('src')
-	  });
-	});
+  /* Main button clicks */
+  $('body').on('click', '#about', function() {
+    $('html, body').animate({
+          scrollTop: $(".hello-world").offset().top
+      }, 500);
+      return false;
+  });
+  
+  $('body').on('click', '#links', function() {
+    $('html, body').animate({
+        scrollTop: $("#links-header").offset().top
+    }, 500);
+    return false;
+  });	
+  
+  $('body').on('click', '#projects', function() {
+    $('html, body').animate({
+        scrollTop: $("#projects-header").offset().top
+    }, 500);
+    return false;
+  });	
+  
+  $('body').on('click', '#contact', function() {
+    $('html, body').animate({
+        scrollTop: $("#contact-header").offset().top
+    }, 500);
+    return false;
+  });	
+  
+  $('body').on('click', '#portfolio', function() {
+    $('html, body').animate({
+        scrollTop: $("#portfolio-header").offset().top
+    }, 500);
+    return false;
+  });	
+  
+  $('body').on('mouseenter mouseleave', 'img.main-photo-1', function() {
+    $('img.main-photo-1').attr({
+      src: $(this).attr('data-photoone-src'), 
+      'data-photoone-src': $(this).attr('src')
+    });
+  });	
+  
+  $('body').on('mouseenter mouseleave', 'img.main-photo-2', function() {
+    $('img.main-photo-2').attr({
+      src: $(this).attr('data-phototwo-src'), 
+      'data-phototwo-src': $(this).attr('src')
+    });
+  });
 });
-
